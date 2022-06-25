e273ac2a0d63eaa81184fc26c5ac68e9fe6c05f8
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-25 00:02:18 -0300

    criando perfil de teste

--- backend/src/main/java/com/augusto/todo/TodoApplication.java
+++ backend/src/main/java/com/augusto/todo/TodoApplication.java
@@ -1,22 +1,12 @@
 package com.augusto.todo;
 
-import com.augusto.todo.domain.Todo;
-import com.augusto.todo.repositories.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
 public class TodoApplication implements CommandLineRunner {
 
-	@Autowired
-	private TodoRepository todoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
@@ -25,10 +15,6 @@ public class TodoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		DateTimeFormatter formatter =DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-		Todo t1 =new Todo(null,"Estudar", "Estudar Spring Boot 2 e Angular 11", LocalDateTime.parse("10/07/2022 10:40",formatter), false);
-
-		todoRepository.saveAll(List.of(t1));
 	}
 }
