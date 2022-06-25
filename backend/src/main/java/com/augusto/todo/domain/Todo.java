ba5c822a19b5270f72a789faae4e42ced1f1804a
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-24 21:58:00 -0300

    iniciando a classe de dominio

---
+++ backend/src/main/java/com/augusto/todo/domain/Todo.java
@@ -0,0 +1,38 @@
+package com.augusto.todo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Todo {
+    private Integer id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataParaFinalizar;
+    private Boolean finalizado = false;
+
+    public Todo() {
+        super();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return Objects.equals(id, todo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
