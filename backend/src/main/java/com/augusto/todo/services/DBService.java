205d6a7409626f9d39767d3442aed5936a14a5f8
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-28 17:12:37 -0300

    arrumando a logica para mostrar as tasks finalizada

--- backend/src/main/java/com/augusto/todo/services/DBService.java
+++ backend/src/main/java/com/augusto/todo/services/DBService.java
@@ -21,7 +21,7 @@ public class DBService {
     public void instaciaBaseDeDados() throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     Todo t1 =new Todo(null,"Estudar", "Estudar Spring Boot 2 e Angular 11", sdf.parse("10/07/2022 "), false);
-    Todo t2 =new Todo(null,"Ler", "Ler livro de desenvolvimento pessoal", sdf.parse("10/07/2022 "), true);
+    Todo t2 =new Todo(null,"Ler", "Ler livro de desenvolvimento pessoal", sdf.parse("10/07/2022 "), false);
     Todo t3 =new Todo(null,"Exercicios", "Fazer flexão", sdf.parse("10/07/2022 "), true);
     Todo t4 =new Todo(null,"Meditar", "Meditar 20 minutos pela manhã", sdf.parse("10/07/2022 "), true);
 
--- frontend/todo/src/app/components/read-all/read-all.component.ts
+++ frontend/todo/src/app/components/read-all/read-all.component.ts
@@ -10,6 +10,9 @@ import { Todo } from 'src/app/models/todo';
 export class ReadAllComponent implements OnInit {
   closed = 0;
   list: Todo[] = []
+
+  listFinished: Todo[] =[];
+
   constructor(private service: TodoService) { }
 
   ngOnInit(): void {
@@ -18,16 +21,16 @@ export class ReadAllComponent implements OnInit {
 
   findAll():void{
     this.service.findAll().subscribe((resposta) =>{
-      this.list = resposta;
-      this.countClosed();
+      resposta.forEach(todo => {
+        if(todo.finalizado){
+          this.listFinished.push(todo);
+        } else{
+          this.list.push(todo);
+        }
+      })
+      this.closed = this.listFinished.length
     })
   }
-
-  countClosed(): void {
-    for(let todo of this.list){
-      if(todo.finalizado){
-        this.closed++;
-      }
-    }
+ 
   }
-}
+
