30668aacc987f109638578cd3c8241e8cf81d421
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-06-29 18:07:53 -0300

    metodo criar novo todo implementado

--- frontend/todo/src/app/components/create/create.component.ts
+++ frontend/todo/src/app/components/create/create.component.ts
@@ -1,3 +1,5 @@
+import { TodoService } from './../../services/todo.service';
+import { Todo } from 'src/app/models/todo';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,14 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  todo: Todo = {
+    titulo: '',
+    descricao: '',
+    dataParaFinalizar: new Date(),
+    finalizado: false
+  }
+
+  constructor(private router: Router, private service: TodoService) { }
 
   ngOnInit(): void {
+  }
+
+  create():void {
+    this.formataData();
+    this.service.create(this.todo).subscribe((resposta) =>{
+      this.service.message('To-do craido com sucesso!');
+      this.router.navigate(['']);
+    }, err =>{
+        this.service.message('Falha ao criar To-do');
+        this.router.navigate(['']);
+    })
+
   }
 
   cancel():void{
     this.router.navigate([''])
   }
-  
+
+  formataData():void{
+    let data = new Date(this.todo.dataParaFinalizar)
+    this.todo.dataParaFinalizar = `${data.getDate()}/${data.getMonth() +1}/${data.getFullYear()} `
+  }
 
 }
