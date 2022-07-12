84eb42d7b1aaf4131b68eb691012c6f1c376ee70
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-12 15:41:09 -0300

    Teste para a classe principal

---
+++ backend/src/test/java/com/augusto/todo/TodoApplicationTest.java
@@ -0,0 +1,14 @@
+package com.augusto.todo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoApplicationTest {
+
+    @Test
+    void main() {
+        TodoApplication.main(new String[]{});
+    }
+
+}
